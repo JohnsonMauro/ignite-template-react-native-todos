a43c3fe0bcbdcf34e8b76a054360de20341679a1
Johnson Mauro <johnsonmauro@gmail.com>
2022-06-18 09:55:48 -0300

    updated

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,4 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  transformIgnorePatterns: [
-    "node_modules/(?!(@react-native|react-native|react-native-vector-icons|react-native-linear-gradient|react-native-iphone-x-helper)/)"
-  ]
 };
