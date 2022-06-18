a92fc870230894df96f1b862ea8bf89767c56ed0
Johnson Mauro <johnsonmauro@gmail.com>
2022-06-18 09:59:31 -0300

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,77 @@
 import React, { useState } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
 
 import { Header } from '../components/Header';
-import { MyTasksList } from '../components/MyTasksList';
+import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
-interface Task {
-  id: number;
-  title: string;
-  done: boolean;
-}
-
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+  function handleEditTask(taskId:number, taskNewTitle:string) {
+    setTasks(oldTasks =>
+      oldTasks.map(task =>
+        task.id === taskId ? { ...task, title: taskNewTitle } : task,
+      ),
+    );
   }
 
-  function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+  function handleAddTask(newTaskTitle: string) {
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    tasks.find(task => task.title === newTaskTitle)
+      ? Alert.alert('Task já cadastrada', "Você não pode cadastrar uma task com o mesmo nome")
+      : setTasks(oldTasks => [...oldTasks, data]);
+  }
+
+  function handleToggleTaskDone(id: number) {
+    setTasks(oldTasks =>
+      oldTasks.map(task =>
+        task.id === id ? { ...task, done: !task.done } : task,
+      ),
+    );
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não"
+        },
+        {
+          text: "Sim", onPress: () => setTasks(oldTasks => oldTasks.filter(
+            task => task.id !== id
+          ))
+        }
+      ]
+    );
   }
 
   return (
-    <>
-      <Header />
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <TasksList
+        tasks={tasks}
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
-    </>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB'
+  }
+})
